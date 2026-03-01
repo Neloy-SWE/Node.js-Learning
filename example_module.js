5d810d4e1efe63c37eef59cee3354c20ead0adaf
Neloy-SWE <taufiqneloy.swe@gmail.com>
2026-03-01 11:31:15 +0600

    strict mode check for commonjs module and Es module

--- example_module.js
+++ example_module.js
@@ -9,6 +9,16 @@ function print() {
     console.log("Hello from example module");
 }
 
+/**
+ * to ensure that commonJs module is operate in non-strict mode or not:
+ * change type in package.json to "commonjs".
+ * change all import and export from ES module to commonJS module in this file and app.js.
+ * uncomment the bellow line and run.
+ * 
+ * though there is no var, let or const keyword, it will not throw error because it is running in non-strict mode. but if we change the type to "module" in package.json, it will throw error because ES module operates in strict mode by default.
+ */
+// nonStrictVariable = 10;
+
 // export properties:
 // console.log(module.exports); // empty object.
 // CommonJS export:
