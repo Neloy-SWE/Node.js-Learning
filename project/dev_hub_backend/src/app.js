c6e49da006d2830c7511f3694c769718b76f9010
Neloy-SWE <taufiqneloy.swe@gmail.com>
2026-03-12 23:13:36 +0600

    signup api on going

--- project/dev_hub_backend/src/app.js
+++ project/dev_hub_backend/src/app.js
@@ -1,7 +1,26 @@
 import express from "express";
 import { connectDB } from "./config/database.js";
+import { User } from "./model/user.js";
 
 const app = express();
+
+app.post("/signup", async (req, res) => {
+    const userObject = {
+        firstName: "Neloy",
+        LastName: "Neel",
+        emailId: "neloy@gmail.com",
+        password: "123456789",
+    }
+
+    const user = new User(userObject);
+    try {
+        await user.save(); // this will return a promise.
+        res.send("User created successfully!");
+    } catch (err) {
+        res.status(400).send("User creation failed!");
+    }
+
+});
 
 connectDB().then(() => {
     console.log("Database connected successfully!");
