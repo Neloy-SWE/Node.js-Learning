13790928f8f2e29dcb5b7dd26ac74689fca6ad70
Neloy-SWE <taufiqneloy.swe@gmail.com>
2026-03-28 15:18:01 +0600

    profile view, edit apis added

--- project/dev_hub_backend/src/route/profile.js
+++ project/dev_hub_backend/src/route/profile.js
@@ -1,13 +1,44 @@
 import express from "express";
 import { userAuthMiddleware } from "../middleware/auth.js";
+import { User } from "../model/user.js";
+import { validatorFields } from "../validator/validator_fields.js";
 
 const profileRouter = express.Router();
 
-profileRouter.get("/profile", userAuthMiddleware, async (req, res) => {
+profileRouter.get("/profile/view", userAuthMiddleware, async (req, res) => {
     try {
         const user = req.user;
         res.send(user);
     } catch (err) {
+        res.status(400).send(err.message);
+    }
+});
+
+profileRouter.patch("/profile/edit", userAuthMiddleware, async (req, res) => {
+    try {
+        const data = req.body;
+        const allowFieldsForUpdates = [
+            "firstName",
+            "lastName",
+            "age",
+            "gender",
+            "about",
+            "skills",
+            "photoUrl",
+        ];
+        validatorFields(data, allowFieldsForUpdates);
+
+        const user = req.user;
+        // console.log(user);
+        Object.keys(req.body).forEach(key => user[key] = req.body[key]);
+        // console.log(user);
+        await user.save();
+
+        // res.send("User updated succesfully!");
+        // we can also send json as response:
+        res.json({ message: "User updated succesfully!", data: user });
+    }
+    catch (err) {
         res.status(400).send(err.message);
     }
 });
--- project/dev_hub_backend/src/validator/validator_fields.js
+++ project/dev_hub_backend/src/validator/validator_fields.js
@@ -1,7 +1,7 @@
 export const validatorFields = (data, allowedFields) => {
     const isValidOperation = Object.keys(data).every((key) => allowedFields.includes(key));
     if (!isValidOperation) {
-        throw new Error("valid fields: " + allowedFields.join(", "));
+        throw new Error("Invalid edit request! ,valid fields: " + allowedFields.join(", "));
     }
     if (data?.skills && data?.skills.length > 10) {
         throw new Error("You can add maximum 10 skills!");
