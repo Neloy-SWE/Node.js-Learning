2549693714a385597f501739784c892eaaf0df6c
Neloy-SWE <taufiqneloy.swe@gmail.com>
2026-03-29 22:50:25 +0600

    connection request schema added

---
+++ project/dev_hub_backend/src/model/connection_request.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const connectionRequestSchema = new mongoose.Schema({
+    fromUserId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+    },
+    toUserId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: {
+            values: ["ignored", "interested", "accepted", "rejected"],
+            message: `{VALUE} is not a valid status`,
+        },
+    }
+}, {
+    timestamps: true
+});
+
+export const ConnectionRequest = mongoose.model("ConnectionRequest", connectionRequestSchema);
