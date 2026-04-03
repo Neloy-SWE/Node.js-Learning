8b4c90206bf72bb4c82261dc55f1df8dbcceb269
Neloy-SWE <taufiqneloy.swe@gmail.com>
2026-04-03 23:41:43 +0600

    login page design on going

--- project/dev_hub_frontend/src/Login.jsx
+++ project/dev_hub_frontend/src/Login.jsx
@@ -1,7 +1,22 @@
 const Login = () => {
     return (
         <>
-        <h1>Hello world</h1>
+            <div className="flex justify-center my-10">
+                <div className="card card-border bg-base-300 w-96 shadow-xl">
+                    <div className="card-body">
+                        <h2 className="card-title">Login</h2>
+                        <div>
+                            <fieldset className="fieldset">
+                                <legend className="fieldset-legend">Email ID</legend>
+                                <input type="text" className="input" placeholder="example@gmail.com" />
+                            </fieldset>
+                        </div>
+                        <div className="card-actions justify-center mt-5">
+                            <button className="btn btn-primary">Login</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
