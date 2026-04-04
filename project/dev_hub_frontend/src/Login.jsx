a83b19ba833b587cbc4ea9045c8492992484d2a3
Neloy-SWE <taufiqneloy.swe@gmail.com>
2026-04-04 17:31:13 +0600

    login page design on going

--- project/dev_hub_frontend/src/Login.jsx
+++ project/dev_hub_frontend/src/Login.jsx
@@ -1,18 +1,40 @@
+import { useState } from "react";
+
 const Login = () => {
+    const [emailId, setEmailId] = useState("");
+    const [password, setPassword] = useState("");
+
     return (
         <>
             <div className="flex justify-center my-10">
                 <div className="card card-border bg-base-300 w-96 shadow-xl">
                     <div className="card-body">
-                        <h2 className="card-title">Login</h2>
+                        <h2 className="card-title justify-center mb-5">Login</h2>
                         <div>
                             <fieldset className="fieldset">
                                 <legend className="fieldset-legend">Email ID</legend>
-                                <input type="text" className="input" placeholder="example@gmail.com" />
+                                <input
+                                    type="text"
+                                    value={emailId}
+                                    className="input w-full" placeholder="example@gmail.com"
+                                    onChange={(e) => setEmailId(e.target.value)}
+                                />
+                            </fieldset>
+                        </div>
+
+                        <div>
+                            <fieldset className="fieldset">
+                                <legend className="fieldset-legend">Password</legend>
+                                <input
+                                    type="password"
+                                    value={password}
+                                    className="input w-full" placeholder="*-*-*-*" 
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    />
                             </fieldset>
                         </div>
                         <div className="card-actions justify-center mt-5">
-                            <button className="btn btn-primary">Login</button>
+                            <button className="btn btn-primary btn-block">Login</button>
                         </div>
                     </div>
                 </div>
