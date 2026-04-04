f730fb2b28236ed24e2140671326e621692bfa6d
Neloy-SWE <taufiqneloy.swe@gmail.com>
2026-04-04 22:32:27 +0600

    redux setup

--- project/dev_hub_frontend/src/App.jsx
+++ project/dev_hub_frontend/src/App.jsx
@@ -1,7 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router";
-import Body from "./Body";
-import Login from "./Login";
-import Profile from "./Profile";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
+import Feed from "./components/Feed";
 
 function App() {
   return (
@@ -9,21 +12,24 @@ function App() {
       {
         // <BrowserRouter> // we can ignore basename also
       }
-      <BrowserRouter basename="/">
-        <Routes>
-          {/* 
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            {/* 
             * self closing tag:
             * <Route path="/" element={<Body />} /> */}
-          <Route path="/" element={<Body />}>
-          {/* 
+            <Route path="/" element={<Body />}>
+              {/* 
             * same as above but with children:
             * 
           */}
-           <Route path="/login" element={<Login />} />
-           <Route path="/profile" element={<Profile />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+              <Route path="/login" element={<Login />} />
+              <Route path="/feed" element={<Feed />} />
+              <Route path="/profile" element={<Profile />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
 
       {/* 
       <h1 className="text-3xl font-bold">
