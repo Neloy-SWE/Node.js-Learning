a0c7e0d5084f80cff9bb25f3bcc3ee8f2a08aa79
Neloy-SWE <taufiqneloy.swe@gmail.com>
2026-04-08 19:42:42 +0600

    feed ui issue fix for empty feed list

--- project/dev_hub_frontend/src/components/Feed.jsx
+++ project/dev_hub_frontend/src/components/Feed.jsx
@@ -25,12 +25,20 @@ const Feed = () => {
         getFeed();
     }, []);
 
-    return feed && (
-        <>
-            <div className="flex justify-center my-10">
-                <UserCard user= {feed[0]}/>
-            </div>
-        </>
+    if (feed === null) {
+        return <h1 className="text-center text-white">Loading...</h1>;
+    }
+
+    if (Array.isArray(feed) && feed.length === 0) {
+        return <h1 className="text-bold text-center text-white text-3xl my-20">
+            No Feed Items Found
+        </h1>;
+    }
+
+    return (
+        <div className="flex justify-center my-10">
+            <UserCard user={feed[0]} />
+        </div>
     );
 }
 
