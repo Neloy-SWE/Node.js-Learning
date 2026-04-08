ee30467827db8ee56adf18b6e22d8e04fa56bc56
Neloy-SWE <taufiqneloy.swe@gmail.com>
2026-04-08 23:33:27 +0600

    sign up feature added

--- project/dev_hub_frontend/src/components/Login.jsx
+++ project/dev_hub_frontend/src/components/Login.jsx
@@ -6,8 +6,13 @@ import { useNavigate } from "react-router";
 import { BASE_URL } from "../utils/constants";
 
 const Login = () => {
-    const [emailId, setEmailId] = useState("jobbar@gmail.com");
-    const [password, setPassword] = useState("#Jobbar12#");
+    const [emailId, setEmailId] = useState("");
+    const [password, setPassword] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+
+    const [isLoginForm, setIsLoginForm] = useState(true);
+
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const user = useSelector((state) => state.user);
@@ -34,10 +39,27 @@ const Login = () => {
         }
     }
 
+    const handleSignUp = async () => {
+        try {
+            const result = await axios.post(BASE_URL + "/signup", {
+                firstName,
+                lastName,
+                emailId,
+                password,
+            }, {
+                withCredentials: true,
+            });
+            dispatch(addUser(result.data.data));
+            navigate("/profile");
+        } catch (err) {
+            setError(err?.response?.data || "Something went wrong");
+        }
+    }
+
     // const path = window.location.pathname;
     // console.log(path);
     const redirection = () => {
-        if (user) {
+        if (user && isLoginForm) {
             navigate("/feed");
             // console.log("feed checked");
         }
@@ -49,11 +71,38 @@ const Login = () => {
 
     return (
         <>
-        
+
             <div className="flex justify-center my-10">
                 <div className="card card-border bg-base-300 w-96 shadow-xl">
                     <div className="card-body">
-                        <h2 className="card-title justify-center mb-5">Login</h2>
+                        <h2 className="card-title justify-center mb-5">{isLoginForm ? "Login" : "Sign Up"}</h2>
+
+                        {!isLoginForm && (<div>
+                            <div>
+                                <fieldset className="fieldset">
+                                    <legend className="fieldset-legend">First Name</legend>
+                                    <input
+                                        type="text"
+                                        value={firstName}
+                                        className="input w-full" placeholder="John"
+                                        onChange={(e) => setFirstName(e.target.value)}
+                                    />
+                                </fieldset>
+                            </div>
+
+                            <div>
+                                <fieldset className="fieldset">
+                                    <legend className="fieldset-legend">Last Name</legend>
+                                    <input
+                                        type="text"
+                                        value={lastName}
+                                        className="input w-full" placeholder="Doe"
+                                        onChange={(e) => setLastName(e.target.value)}
+                                    />
+                                </fieldset>
+                            </div>
+                        </div>)}
+
                         <div>
                             <fieldset className="fieldset">
                                 <legend className="fieldset-legend">Email ID</legend>
@@ -79,8 +128,10 @@ const Login = () => {
                         </div>
                         <p className="text-error">{error}</p>
                         <div className="card-actions justify-center mt-5">
-                            <button className="btn btn-primary btn-block" onClick={handleLogin}>Login</button>
+                            <button className="btn btn-primary btn-block" onClick={isLoginForm ? handleLogin : handleSignUp}>{isLoginForm ? "Login" : "Sign Up"}</button>
                         </div>
+
+                        <p className="text-center mt-3 cursor-pointer" onClick={() => setIsLoginForm((value) => !value)}>{isLoginForm ? "New user? Sign up here" : "Existing user? Login here"}</p>
                     </div>
                 </div>
             </div>
