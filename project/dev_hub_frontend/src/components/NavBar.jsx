051aa53fd85fc6b923f523cd36d09681484c7a1f
Neloy-SWE <taufiqneloy.swe@gmail.com>
2026-04-08 15:22:04 +0600

    connection list page added

--- project/dev_hub_frontend/src/components/NavBar.jsx
+++ project/dev_hub_frontend/src/components/NavBar.jsx
@@ -45,14 +45,13 @@ const NavBar = () => {
                             </div>
                             <ul
                                 tabIndex="-1"
-                                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                                className="menu menu-sm dropdown-content bg-base-300 rounded-box z-1 mt-3 w-52 p-2 shadow">
                                 <li>
                                     <Link to="/profile" className="justify-between">
                                         Profile
-                                        <span className="badge">New</span>
                                     </Link>
                                 </li>
-                                <li><a>Settings</a></li>
+                                <li><Link to="/connections">Connections</Link></li>
                                 <li><a onClick={handleLogout}>Logout</a></li>
                             </ul>
                         </div>
